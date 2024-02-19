0abc7572ba7a2f83063a36169c550ec3f103e898
Sarthak Chaure <sarchaure911@gmail.com>
2024-02-19 13:58:24 +0530

    Added cv , readme and req

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Detection of E-Waste using OpenCV and YOLOv8
+
+Trained YOLOv8 on a dataset of electronics from Google's Open Image Dataset for detection and segmentation. The model is able to identify various types of electronic waste, including mobile phone, computer mouse, computer keyboard, printer, computer monitor, headphones.
+This project is an implementation of the object detection model, YOLOv8 for detecting electronic  waste (E-waste) in images. The code uses OpenCV to read, preprocess and visualize the results from the model with an border around the detected object with an cofidence percentage.
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+
+## Installation
+
+Run the following commands to install the required libraries.
+```bash
+pip install -r requirements.txt
+```
+
+[Link to the prepared dataset](#https://drive.google.com/drive/folders/1YBtsNDSrWJjjTbAtVTglOW_B_BOz2-eY?usp=sharing)
+
+## usage
+
+To run the CV , simply run the following command.
+```bash
+python main.py
+```
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2 as cv
 from ultralytics import YOLO
-from google.colab.patches import cv2_imshow
+#from google.colab.patches import cv2_imshow
+
+model = YOLO('yolov8n.pt')
+model = YOLO('best.pt')
+  
+
 def detect(image):
-  model = YOLO('yolov8n.pt')
-  model = YOLO('/content/runs/detect/eWaste2/weights/best.pt')
-
   results = model.predict(image)
-
-  image = cv.imread(image)
+  #image = cv.imread(image)
   for box in results[0].boxes:
     class_id = results[0].names[box.cls[0].item()]
     cords = box.xyxy[0].tolist()
@@ -19,9 +20,20 @@ def detect(image):
     cv.rectangle(image, (x1, y1), (x2, y2), color, thickness)
     text = f'Object: {class_id} --- Confidence: {conf}'
     cv.putText(image, text, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    #cv.putText(image,"Predd D to Exit",(200,200), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+  return  image
 
-  cv2_imshow(image)
-  # cv.imshow(image)
-  # cv.waitKey(0)
-  # cv.destroyAllWindows()
+video=cv.VideoCapture(0)
+
+while True:
+  ret , frame = video.read()
+  frame=detect(frame)
+  #cv2_imshow(image)
+  
+  cv.imshow("E-Waste Detection",frame)
+  
+  if cv.waitKey(1)&0xFF==ord('d'):
+    break
+video.release()
+cv.destroyAllWindows()
 
